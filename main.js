d33b061462cebab914433380da5fc500b0f6c815
rayankhan01 <rk12172002@gmail.com>
2024-03-09 20:26:17 +0500

    Your own array

---
+++ main.js
@@ -0,0 +1,2 @@
+var transports = ["Bugatti Chiron", "Audi RS7", "Honda Civic RS"];
+transports.forEach(function (transport) { console.log("I would like to own a ".concat(transport, ".")); });
---
+++ main.ts
@@ -0,0 +1,2 @@
+let transports: string[] = ["Bugatti Chiron" , "Audi RS7" , "Honda Civic RS"];
+transports.forEach(transport => {console.log(`I would like to own a ${transport}.`);});
